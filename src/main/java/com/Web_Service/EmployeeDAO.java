c4e2638b3cb440a0250f28c4ae35fbbdeba7317b
Habte Selassie <habtuchelsea1000@gmail.com>
2023-12-05 10:23:40 +0300

    adding DAO class to encapsulate db

---
+++ src/main/java/com/Web_Service/EmployeeDAO.java
@@ -0,0 +1,78 @@
+package com.Web_Service;
+
+import org.springframework.stereotype.Repository;
+
+import com.Web_Service.Employees;
+
+@Repository
+
+public class EmployeeDAO {
+
+    private static Employees list = new Employees();
+
+    static {
+
+        list.getEmployeeList().add(
+          new Employee(
+              1,
+             "Habte Selassie",
+             "Fitsum",
+              19,
+             "habtuchelsea1000@gmail.com",
+             "Software Engineer"
+            ));
+
+
+             list.getEmployeeList().add(
+          new Employee(
+              1,
+             "Israel",
+             "Fitsum",
+              19,
+             "israel@gmail.com",
+             "Data Engineer"
+            ));
+
+             list.getEmployeeList().add(
+          new Employee(
+              1,
+             "Bezawit",
+             "Fitsum",
+              19,
+             "bezawit@gmail.com",
+             "Software Engineer"
+            ));
+
+
+             list.getEmployeeList().add(
+          new Employee(
+              1,
+             "Girum",
+             "Fitsum",
+              19,
+             "habtuchelsea1000@gmail.com",
+             "Software Engineer"
+            ));
+
+        list.getEmployeeList().add(
+           new Employee(
+             1,
+             "Nathnael",
+             "Getachew",
+              19,
+             "nati@gmail.com",
+             "Waiter"
+            ));
+            
+  
+    }
+
+    public Employees getEmployees() {
+        return list;
+    }
+
+
+    public void addEmployee(Employee employee) {
+        list.getEmployeeList().add(employee);
+    }
+}
