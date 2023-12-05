604964597cb6a309fa98144c6b0348deb3575e5a
Habte Selassie <habtuchelsea1000@gmail.com>
2023-12-05 19:17:34 +0300

    making the update,create,delete and get method

--- src/main/java/com/Project/Web_Service/EmployeeManager.java
+++ src/main/java/com/Project/Web_Service/EmployeeManager.java
@@ -3,10 +3,13 @@ package com.Project.Web_Service;
 import org.springframework.stereotype.Repository;
 
 import com.Project.Web_Service.Employees;
+import com.Project.Web_Service.Employee;
 
 @Repository
 
 public class EmployeeManager {
+
+    
 
     private static Employees list = new Employees();
 
@@ -68,8 +71,90 @@ public class EmployeeManager {
     }
 
     public Employees getAllEmployees() {
-        return list;
+         /////////////// check if employye exist
+         if (list == null) {
+             System.out.println("Employees never exist");
+         }
+        return  list;
     }
+
+    public Employee getEmployee(Integer id) {
+        /////////////// check if employee id is appropriate
+        if( list == null || id < 0 || id > list.getEmployeeList().size()) {
+              System.out.println("Employee never exist or multiple employees db never exist");
+              return null;
+        }
+
+        Employee singleEmployee = list.getEmployeeList().get(id);
+
+         if (singleEmployee == null ) {
+             System.out.println("Employee never exist");
+         }
+         return singleEmployee;
+    }
+
+
+
+
+     public Employees createEmployee(Employee employee, Integer id ) {
+        /////////////// check if employye exist
+        if (list == null) {
+/////// if the list db is empty then u have to create a db and make its id to 1 or first
+           Integer firstId = list.getEmployeeList().size() + 1;
+            employee.setId(firstId);
+
+            list = new Employees();
+            
+            list.employeeList.add(employee);
+
+        }
+
+/////// if some list db is there then u have to append a created  db and make its id after the previosu db
+        else {
+                // list.getEmployeeList().add(employee); 
+                Integer newId = list.getEmployeeList().size() + 1;
+                employee.setId(newId);
+
+                list.getEmployeeList().add(employee); 
+            }
+        
+         return list;
+         
+    }
+
+     public Employees updateEmployee(Integer id, Employee updatedEmployee) {
+         /////////////// check if employye exist
+         if ( list == null || id < 0 || id > list.getEmployeeList().size()) {
+             System.out.println("list is zero or the id of the db is less than zero or greater than db ");
+             return null;
+         }
+
+        else if (list.getEmployeeList().get(id) == null) {
+             System.out.println("Employe not found in the list");
+         }
+
+         else {
+
+            list.getEmployeeList().set(id, updatedEmployee);
+           
+         }
+
+         return list;
+    }
+
+     public Employees deleteEmployee(Integer id, Employee deletedEmployee) {
+         /////////////// check if employye exist
+            if (list.getEmployeeList().get(id) == null) {
+              System.out.println("Employee not found in the list");
+          }
+
+           else {
+              list.getEmployeeList().remove(deletedEmployee);
+           }
+           return list;
+    }
+
+     
 
 
     public void addEmployee(Employee employee) {
