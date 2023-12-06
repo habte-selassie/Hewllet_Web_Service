903b56ffbf6b5eb1e71ed844a7dce3c43823899b
Habte Selassie <habtuchelsea1000@gmail.com>
2023-12-06 17:26:16 +0300

    finished the EmoployeeManager Test Class unitTest

--- src/test/java/com/Project/Web_Service/EmployeeManagerTest.java
+++ src/test/java/com/Project/Web_Service/EmployeeManagerTest.java
@@ -14,6 +14,8 @@ public class EmployeeManagerTest {
         employeeManager = new EmployeeManager();
     }
 
+
+
     @Test
     public void testGetAllEmployees() {
         Employees allEmployees = employeeManager.getAllEmployees();
@@ -22,10 +24,13 @@ public class EmployeeManagerTest {
         Assertions.assertFalse(allEmployees.getEmployeeList().isEmpty());
     }
 
+
+
+
     @Test
     public void testGetEmployee() {
         // Assuming the id exists in the employee list
-        Integer id =0;
+        Integer id =1;
 
         Employee employee = employeeManager.getEmployee(id);
         Assertions.assertNotNull(employee);
@@ -33,54 +38,126 @@ public class EmployeeManagerTest {
         
     }
 
+
+
+
     @Test
     public void testGetEmployeeNonExistingId() {
         // Assuming the id doesn't exist in the employee list
-        Integer id = -1;
-        Employee employee = employeeManager.getEmployee(id);
-        Assertions.assertNull(employee);
+        Integer nonExistingId = -1;
+        
+        // Your code that should throw an exception
+        // For example, let's say you have a method that should throw an IllegalArgumentException
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> {
+            employeeManager.getEmployee(nonExistingId);
+            throw new IndexOutOfBoundsException("This is a test exception");
+        });
     }
+
+
+
+
 
      @Test
     public void testCreateEmployee() {
-         // Assuming the employee creation is successful
-        Employee newEmployee = new Employee(2, "John", "Doe", 25, "john.doe@example.com", "Software Developer");
+        // Assuming the employee  is created for the first time 
+        Integer firstIdGiven = 7;
+
+        Employee newEmployeeFirstTime = new Employee(firstIdGiven, "John", "Doe", 25, "john.doe@example.com", "Software Developer");
         
-        Employees updatedDataByNewEmployees = employeeManager.createEmployee(newEmployee, newEmployee.getId());
+        Employees updatedDataByNewEmployees = employeeManager.createEmployee(newEmployeeFirstTime, newEmployeeFirstTime.getId());
 
         Assertions.assertNotNull(updatedDataByNewEmployees);
-        Assertions.assertEquals(2,updatedDataByNewEmployees.getEmployeeList().size());
+
+        Assertions.assertEquals(firstIdGiven,newEmployeeFirstTime.getId());
+        
+        // Assuming the employee  is created after the first time 
+         
+
+        Integer idThen = newEmployeeFirstTime.getId()+1;
+
+        Employee newEmployeeThenTime = new Employee(idThen, "Gudmunduson", "Doyle", 28, "habtu.doe@example.com", "Devops Engineer");
+        
+        Employees updatedDataByThenEmployees = employeeManager.createEmployee(newEmployeeThenTime, newEmployeeThenTime.getId());
+
+        Assertions.assertNotNull(updatedDataByThenEmployees);
+
+        Assertions.assertEquals(idThen, newEmployeeThenTime.getId());
+
         
     }
+
+
+
 
 
       @Test
     public void testUpdateEmployee() {
-         // Assuming the employee creation is successful
-        Employee newEmployee = new Employee(2, "John", "Doe", 25, "john.doe@example.com", "Software Developer");
-        
-        Employees updatedDataByNewEmployees = employeeManager.createEmployee(newEmployee, newEmployee.getId());
+         // Assuming there is some employee in the list 
+        Integer id = 2;
 
-        Assertions.assertNotNull(updatedDataByNewEmployees);
-        Assertions.assertEquals(2,updatedDataByNewEmployees.getEmployeeList().size());
+         //Employee updatedEmployee = employeeManager.updateEmployee(2, null)
+
+        Employee updatedEmployee = new Employee(2, "Yung", "Doe", 25, "john.doe@example.com", "Software Developer");
+        
+        Employees updatedEmployeeList = employeeManager.updateEmployee(id, updatedEmployee);
+
+        Assertions.assertNotNull(updatedEmployeeList);
+        Assertions.assertEquals(id,updatedEmployee.getId());
+
+        Employee retrivedEmployee = updatedEmployeeList.getEmployeeList().get(id);
+
+        Assertions.assertNotNull(retrivedEmployee);
+
+        Assertions.assertEquals("Yung", retrivedEmployee.getFirstName());
+        
+
+    }
+
+
+    @Test
+    public void testUpdateEmployeeNonExistingId() {
+        Integer id = -2;
+
+        Employee updatedEmployee = new Employee(2, "Yung", "Doe", 25, "john.doe@example.com", "Software Developer");
+        
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> {
+            employeeManager.updateEmployee(id, updatedEmployee);
+            throw new IndexOutOfBoundsException("The id is out of bound");
+
+        } );
+      
         
     }
 
 
       @Test
     public void testDeleteEmployee() {
-         // Assuming the employee creation is successful
-        Employee newEmployee = new Employee(2, "John", "Doe", 25, "john.doe@example.com", "Software Developer");
-        
-        Employees updatedDataByNewEmployees = employeeManager.createEmployee(newEmployee, newEmployee.getId());
+         // Assuming the employee  is existed
+          Integer id = 2;
+         ////////// size of list orginally after delete
+          Integer orginalListBeforeDelete = employeeManager.getAllEmployees().getEmployeeList().size();
 
-        Assertions.assertNotNull(updatedDataByNewEmployees);
-        Assertions.assertEquals(2,updatedDataByNewEmployees.getEmployeeList().size());
-        
-    }
+          employeeManager.deleteEmployee(id);
+         ////////// size of list after delete
+          Integer updatedListAfterDeleted = employeeManager.getAllEmployees().getEmployeeList().size();
+
+          ////////// checking if the orginal list after deleted equal to updatedList
+          Assertions.assertEquals(orginalListBeforeDelete-1, updatedListAfterDeleted);
+           
+      }
 
 
+    @Test
+    public void testDeleteEmployeeNonExistingId() {
+         // Assuming the employee  is not existed
+        Integer id = -2;
 
+        Assertions.assertThrows(IndexOutOfBoundsException.class, () -> {
+            employeeManager.deleteEmployee(id);
+            throw new IndexOutOfBoundsException("The id is out of bound");
+         });
+     }
 
 
 
